aad6df9fc452351c0e899ef6a3e056be6dede509
nedanemati <74539243+nedanemati@users.noreply.github.com>
2022-06-01 16:34:06 +0430

    added icon

--- src/components/ui/Estimate.jsx
+++ src/components/ui/Estimate.jsx
@@ -3,6 +3,15 @@ import Lottie from "lottie-web";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+
+import backArrow from "../../assets/backArrow.svg";
+import forwardArrow from "../../assets/forwardArrow.svg";
+import chech from "../../assets/check.svg";
+import mobile from "../../assets/mobile.svg";
+import send from "../../assets/send.svg";
+import website from "../../assets/website.svg";
+import software from "../../assets/software.svg";
 
 function Estimate() {
   return <div></div>;
